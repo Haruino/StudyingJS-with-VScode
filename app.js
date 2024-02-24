11645659e9963337835606116f2fd4deb62c5f60
Haruino <harukiinoue5270@gmail.com>
2024-02-24 21:10:45 +0900

    [Finish]JS-2

---
+++ app.js
@@ -0,0 +1,20 @@
+let max = 100;
+let num = 0.01;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+// do...while文では、条件式のtrue／falseに関わらず、最初の1回だけは必ず処理が行われる
+
+let i;
+let num2 = 0;
+
+for(i = 1; i < 11; i++){
+  num2 = num2 + i;
+}
+
+alert('1から10まで足し算した結果は' + num2 + 'です');
