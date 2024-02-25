55dfac25579b00b16acc3a350bee063290cf2744
Haruino <harukiinoue5270@gmail.com>
2024-02-25 14:51:19 +0900

    [FInish]JS-6

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function (){
+  $('.box1').slideDown(1000, function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(1000);
+  });
 });
+
+  /* slideDown()の引数にfunctionを書き加えることで、スライドダウンした後に
+  functionを実行することができるようになる */
+
+
+  $(function(){
+    $('.box2').on('click', function(){
+      $('.box2').addClass('box2-ext');
+    });
+    $('.box2').mouseout('click', function(){
+      $('.box2').removeClass('box2-ext');
+    });
+  });
